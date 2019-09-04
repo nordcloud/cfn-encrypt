e0dd2567014d438114b84f7fc875ac8b0dbdeeed
Tom Sleebe <tom@sabern.com>
2019-09-04 15:20:27 +0200

    Merge ssm_parameter_generatepassword into ssm_parameter file

--- cfn_encrypt/ssm_parameter.py
+++ cfn_encrypt/ssm_parameter.py
@@ -1,4 +1,6 @@
 import cfnresponse, logging, traceback, boto3
+from random import choice
+from string import ascii_uppercase, ascii_lowercase, digits
 
 
 def parameter_exist(name):
@@ -19,16 +21,46 @@ def handler(event, context):
     ch = logging.StreamHandler()
     logger.addHandler(ch)
     name = event["ResourceProperties"]["Name"]
-    try:
+    value = None
 
+    try:
         if event["RequestType"] in ["Create", "Update"]:
             if event["RequestType"] == "Create" and parameter_exist(name):
                 raise NameError("A Parameter named {} already exists".format(name))
 
+            generate_password = event["ResourceProperties"]["GeneratePassword"] if "GeneratePassword" in event["ResourceProperties"] else None
+            value = event["ResourceProperties"]["Value"] if "Value" in event["ResourceProperties"] else None
+
+            if value and generate_password in ['true', 'True', '1', True, 1]:
+                raise ValueError("Property Value and GeneratePassword cannot be used at the same time")
+
+            if generate_password in ['true', 'True', '1', True, 1]:
+
+                password_length = event["ResourceProperties"]["GeneratePasswordLength"] if "GeneratePasswordLength" in event["ResourceProperties"] else None
+                allow_specials = event["ResourceProperties"]["GeneratePasswordAllowSpecialCharacters"] if "GeneratePasswordAllowSpecialCharacters" in event["ResourceProperties"] else None
+                if not password_length:
+                    raise ValueError("The Resource property GeneratePasswordLength is required")
+
+                try:
+                    password_length = int(password_length)
+                except:
+                    raise ValueError("The Resource property GeneratePasswordLength must be an integer or castable to an integer")
+
+
+                charset = ascii_uppercase + ascii_lowercase + digits
+                if allow_specials and allow_specials in ['true', 'True', '1', True, 1]:
+                    charset = charset + "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+                value = ''.join(choice(charset) for i in range(password_length))
+
+            if not value:
+                raise ValueError("Either generate a password or set a value")
+
+            print(value)
             response = boto3.client('ssm').put_parameter(
                 Name=name,
                 Description=event["ResourceProperties"]["Description"],
-                Value=event["ResourceProperties"]["Value"],
+                Value=value,
                 Type="SecureString",
                 KeyId=event["ResourceProperties"]["KeyId"],
                 Overwrite=True
@@ -45,7 +77,7 @@ def handler(event, context):
             cfnresponse.send(event, context, cfnresponse.SUCCESS, None, name)
 
     except Exception as ex:
-        logger.error("Faild to %s parameter: %s", event["RequestType"], name)
+        logger.error("Failed to %s parameter: %s", event["RequestType"], name)
         logger.debug("Stack trace %s", traceback.format_exc())
         if event["RequestType"] in ["Create", "Update"]:
             cfnresponse.send(event, context, cfnresponse.FAILED, None, "0")
